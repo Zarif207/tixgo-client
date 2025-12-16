5439f5fda70c767e3e03f5c225f37f6a3d14ef0e
Zarif207 <zarifhasan207@gmail.com>
2025-12-16 23:30:15 +0600

    added useRole hook

--- src/Hooks/UseRole.jsx
+++ src/Hooks/UseRole.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import UseAuth from "./UseAuth";
+import UseAxiosSecure from "./UseAxiosSecure";
 
 const UseRole = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { user } = UseAuth();
+  const axiosSecure = UseAxiosSecure();
+
+  const [role, setRole] = useState(null);
+  const [roleLoading, setRoleLoading] = useState(true);
+
+  useEffect(() => {
+    if (!user?.email) {
+      setRoleLoading(false);
+      return;
+    }
+
+    axiosSecure
+      .get(`/users/role?email=${user.email}`)
+      .then((res) => {
+        setRole(res.data.role); 
+        setRoleLoading(false);
+      })
+      .catch(() => {
+        setRole(null);
+        setRoleLoading(false);
+      });
+  }, [user, axiosSecure]);
+
+  return { role, roleLoading };
 };
 
 export default UseRole;
